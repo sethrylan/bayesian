b53e7c19ca584b0fb1f40da2fb1737a0dd741aec
sethrylan <seth.rylan@gmail.com>
2013-08-07 16:01:25 -0400

    Removed duplicate quiz logic.

--- client/js/calibrate.js
+++ client/js/calibrate.js
@@ -12,9 +12,16 @@ $("input").keypress(function (e) {
 });
 
 /*
- *  Main quiz function. Parameters numQuestions, progressPixels and answers must be defined.
+ *  Main quiz function. Parameters progressPixels and answers must be defined.
  */
 $(function(){
+	var totalQuestions = $('.questionContainer').size();
+	var currentQuestion = 0;
+	$questions = $('.questionContainer');
+	$questions.hide();
+	$($questions.get(currentQuestion)).fadeIn();
+	
+
 	/*
 	$( "#area-slider" ).slider({
 		range: true,
@@ -29,6 +36,40 @@ $(function(){
 	*/
 
 	var jQuiz = {
+		finish: function() {
+			jQuiz.userAnswers = [];						
+			
+			var lows = $('input[name$="low"]').map(function() { return $(this).val() }).get();
+			var highs = $('input[name$="high"]').map(function() { return $(this).val() }).get();
+			var confidences = $('input[name$="confidence"]').map(function() { return $(this).val() }).get();
+			
+			assert((highs.length === lows.length) && (lows.length === confidences.length), "Answer sizes must match.");
+			
+			for (var i = 0; i < highs.length; i++) {
+				var userAnswer = { 
+					n: i + 1,
+					low: lows[i],
+					high: highs[i],
+					confidence: confidences[i]
+				};
+
+				jQuiz.userAnswers.push(userAnswer);
+			}						
+			
+			//$('#progress').width(300);
+			//$('#progressContainer').hide();
+			var results = jQuiz.checkAnswers();
+			var resultSet = '';
+			var trueCount = 0;
+			for (var i = 0, ii = results.length; i < ii; i++){
+				if (results[i] == true) trueCount++;
+				resultSet += '<div> Question ' + (i + 1) + ' is ' + results[i] + '</div>'
+			}
+			resultSet += '<div class="totalScore">Your total score is ' + parseInt(trueCount * (100/totalQuestions), 10) + ' / 100</div>'
+			$('#resultContainer').html(resultSet).show();
+			$('.buttonContainer').hide();
+
+		},
 		checkAnswers: function() {					
 			var resultArr = [];
 			for (var key in this.userAnswers) {		
@@ -50,45 +91,22 @@ $(function(){
 					return false;
 				}
 				$(this).addClass('disabled');		
-				$(this).parents('.questionContainer').fadeOut(500, function() {
-					$(this).next().fadeIn(500);
-					$(this).removeClass('disabled')
+				$($questions.get(currentQuestion)).fadeOut(500, function() {
+					currentQuestion = currentQuestion + 1;
+					if( currentQuestion == totalQuestions ){
+						jQuiz.finish();
+					} else {
+						$($questions.get(currentQuestion)).fadeIn(500);
+						$('.next').removeClass('disabled')
+						if( currentQuestion == totalQuestions-1 ) {
+							$('.next').text('| Finish |');
+						}
+					}
 				});
+
 				var el = $('#progress');
 				el.width(el.width() + progressPixels + 'px');
-			});
-			$('.finish').click(function(){					
-				jQuiz.userAnswers = [];						
-				
-				var lows = $('input[name$="low"]').map(function() { return $(this).val() }).get();
-				var highs = $('input[name$="high"]').map(function() { return $(this).val() }).get();
-				var confidences = $('input[name$="confidence"]').map(function() { return $(this).val() }).get();
-				
-				assert((highs.length === lows.length) && (lows.length === confidences.length), "Answer sizes must match.");
-				
-				for (var i = 0; i < highs.length; i++) {
-					var userAnswer = { 
-						n: i + 1,
-						low: lows[i],
-						high: highs[i],
-						confidence: confidences[i]
-					};
-
-					jQuiz.userAnswers.push(userAnswer);
-				}						
-				
-				//$('#progress').width(300);
-				//$('#progressContainer').hide();
-				var results = jQuiz.checkAnswers();
-				var resultSet = '';
-				var trueCount = 0;
-				for (var i = 0, ii = results.length; i < ii; i++){
-					if (results[i] == true) trueCount++;
-					resultSet += '<div> Question ' + (i + 1) + ' is ' + results[i] + '</div>'
-				}
-				resultSet += '<div class="totalScore">Your total score is ' + parseInt(trueCount * (100/numQuestions), 10) + ' / 100</div>'
-				$('#resultContainer').html(resultSet).show();
-			})
+			});			
 		}
 	};
 	jQuiz.init();
