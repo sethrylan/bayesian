bed479d2295b918eea86e9b2535d8811e85a506f
Seth Rylan <seth.rylan@gmail.com>
2013-08-18 11:58:14 -0400

    Added functional tests.

---
+++ server/src/functionalTest/java/org/lenition/functional/FactbookServletTest.java
@@ -0,0 +1,32 @@
+package org.lenition.functional;
+
+import org.junit.Test;
+import org.lenition.domain.Quiz;
+
+import static com.jayway.restassured.RestAssured.expect;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+
+public class FactbookServletTest {
+
+    @Test
+    public void testQuestionsSize() {
+        final int parameter = 10;
+        expect().
+            body("questions.size", equalTo(parameter)).
+            when().
+            get("/factbook/questions?n={n}", String.valueOf(parameter));
+    }
+
+    @Test
+    public void testNonNullFields() {
+        final int parameter = 100;
+        expect().
+            body("questions", not(containsString(" null"))).
+            when().
+            get("/factbook/questions?n={n}", String.valueOf(parameter));
+    }
+
+
+}
