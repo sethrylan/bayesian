583ce4a45edde5392d0e877bd6655fce0921a945
Seth Rylan <seth.rylan@gmail.com>
2013-08-20 11:33:03 -0400

    Formatted citations.

--- client/js/citation.js
+++ client/js/citation.js
@@ -1,33 +1,28 @@
 $(document).ready(function() {
 
-
+    // Create lists of sources and citations elements
     $sources = $('.csl-entry');
     $citations = $('.cite');
 
-    /*
-    $sources.attr('id', function( arr ) {
-        return 'source-' + arr;
-    });
-    */
-
+    // For each citation, find its corresponding source (citation.title -> source.id)
     $.each( $citations, function( key, citation ) {
-
         var source = $sources.filter(function(index) {
             return $(this).attr('id') == citation.title;
         });
         
-        $(this).attr('title', source.html());
-        $(this).text('[' + $sources.index(source) + ']');
+        // Set the citation's title (used for tooltip) to the source's content. If text already existed for the citation (such as a page number), it is appended.
+        $(this).attr('title', source.html() + $(this).html());
 
+        // Set the citations visible text to the index of the source (plus one for zero-indexed list)
+        $(this).text('[' + ($sources.index(source) + 1) + ']');
     });
 
-
-    $('.cite').qtip({
+    $('.cite, .note').qtip({
         hide: {
             fixed: true,
             delay: 300
         },
-        content: function() {
+        content: function() { 
             return $(this).attr('title');
         },
         style: {
