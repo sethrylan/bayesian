022ab183abdb31a0a2e31aee8cb7e102646c48f9
Seth Rylan <seth.rylan@gmail.com>
2013-08-21 14:26:44 -0400

    Removed ugly stream utility methods.

--- server/src/main/java/org/lenition/servlet/FactbookServlet.java
+++ server/src/main/java/org/lenition/servlet/FactbookServlet.java
@@ -1,10 +1,12 @@
 package org.lenition.servlet;
 
 import com.google.gson.Gson;
+import org.apache.commons.io.IOUtils;
 import org.lenition.singleton.FactbookQuiz;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
 
 /**
  * ReST resource for factbook questions
@@ -19,7 +21,12 @@ public class FactbookServlet {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String factbook() {
-        return convertStreamToString(FactbookServlet.class.getClassLoader().getResourceAsStream("factbook-countries.json"));
+        try {
+            return IOUtils.toString(FactbookServlet.class.getClassLoader().getResourceAsStream("factbook-countries.json"), "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "{ \"error\" : \"Could not find factbook\" }";
+        }
     }
 
     /**
@@ -32,11 +39,6 @@ public class FactbookServlet {
     @Produces(MediaType.APPLICATION_JSON)
     public String question(@DefaultValue("30") @QueryParam("n") int n) {
         return (new Gson()).toJson(FactbookQuiz.INSTANCE.getQuestions(n));
-    }
-
-    static String convertStreamToString(java.io.InputStream is) {
-        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
-        return s.hasNext() ? s.next() : "";
     }
 
 }
