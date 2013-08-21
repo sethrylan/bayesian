fc819af3901824c83bb9672f670e8d1b99079466
Seth Rylan <seth.rylan@gmail.com>
2013-08-21 14:59:42 -0400

    Added simple ReST class for persistence.

--- persistence/src/main/java/org/lenition/servlet/JsonDataStoreServlet.java
+++ persistence/src/main/java/org/lenition/servlet/JsonDataStoreServlet.java
@@ -1,13 +1,17 @@
 package org.lenition.servlet;
 
+import com.google.appengine.api.datastore.*;
+import com.google.gson.Gson;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.Map;
+import java.util.UUID;
 
 @Path("/")
 public class JsonDataStoreServlet {
 
-//    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -18,27 +22,31 @@ public class JsonDataStoreServlet {
     @PUT
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public String put(Map<String, String> map) {
+    public String put(String json) {
 
         /*
         see https://developers.google.com/appengine/docs/java/gettingstarted/usingdatastore
         https://developers.google.com/appengine/docs/java/datastore/entities
         */
 
+        Key entityKey = KeyFactory.createKey("QuizComplete", UUID.randomUUID().toString());
+        Entity quizEntity = new Entity(entityKey);
+        Gson gson = new Gson();
+        Map map = gson.fromJson(json, Map.class);
+        for(Object key : map.keySet()) {
+            System.out.println(key.toString() + " : " + map.get(key).toString());
+        }
 
+//        jsonArray.
 //        for (String key : map.keySet()) {
 //            String value = map.get(key).trim();
-//            if (value.length()>0 && value.length()<450) {  // Short String - store as is
-////                movieEntity.setProperty(key, value);
+//            if (value.length() > 0 && value.length() < 450) {  // Short String - store as is
+//                quizEntity.setProperty(key, value);
 //            } else if (value.length()>=450) {              // Long String - convert to text
-////                movieEntity.setProperty(key, new Text(value));
+//                quizEntity.setProperty(key, new Text(value));
 //            }
 //        }
-//
-//        return new Entity("test");
-
-        return "{ \"persistence\" : \"put\" }";
-
+        Key persistedKey = datastore.put(quizEntity);
+        return "{ \"complete\" : \"" + (persistedKey.isComplete() ? "true" : "false") + "\" }";
     }
-
 }
