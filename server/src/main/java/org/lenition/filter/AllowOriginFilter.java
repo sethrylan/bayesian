39bbd28499ee9373f986a7d419e48525ba01a03a
Seth Rylan <seth.rylan@gmail.com>
2013-08-21 22:05:09 -0400

    javadoc added.

--- server/src/main/java/org/lenition/filter/AllowOriginFilter.java
+++ server/src/main/java/org/lenition/filter/AllowOriginFilter.java
@@ -4,8 +4,17 @@ import com.sun.jersey.spi.container.ContainerRequest;
 import com.sun.jersey.spi.container.ContainerResponse;
 import com.sun.jersey.spi.container.ContainerResponseFilter;
 
+/**
+ * Jersey filter to allow CORS
+ */
 public class AllowOriginFilter implements ContainerResponseFilter {
 
+    /**
+     * Add Access-Control-Allow-* headers to response
+     * @param request request with headers
+     * @param response response to add headers to
+     * @return response with modified headers
+     */
     @Override
     public ContainerResponse filter(ContainerRequest request, ContainerResponse response) {
         response.getHttpHeaders().putSingle("Access-Control-Allow-Origin", "*");
