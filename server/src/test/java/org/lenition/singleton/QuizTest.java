cf557f4f5849dcfbcca1bac606f62c043715c482
Seth Rylan <seth.rylan@gmail.com>
2013-08-22 17:18:29 -0400

    Test cleanup.

--- server/src/test/java/org/lenition/singleton/QuizTest.java
+++ server/src/test/java/org/lenition/singleton/QuizTest.java
@@ -3,9 +3,13 @@ package org.lenition.singleton;
 import com.google.gson.Gson;
 import org.lenition.domain.Quiz;
 
+import java.util.logging.Logger;
+
 import static org.junit.Assert.assertTrue;
 
 public class QuizTest {
+
+    private static final Logger log = Logger.getLogger(QuizTest.class.getName());
 
     @org.junit.Test
     public void testGetQuestions() {
@@ -13,7 +17,7 @@ public class QuizTest {
         for(int i = 1; i <= 40; i++) {
             Quiz quiz = FactbookQuiz.INSTANCE.getQuestions(i);
             String json = (new Gson()).toJson(quiz);
-            System.out.println(json);
+            //log.info(json);
             Quiz quiz2 = gson.fromJson(json, org.lenition.domain.Quiz.class);
             assertTrue(quiz2.questions.length == i);
         }
