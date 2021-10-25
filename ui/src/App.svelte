a66964ea2774de0e66fa4f3bfed2a0a5cefcfbfb
Seth Rylan Gainey <seth@apothesource.com>
2021-10-25 09:55:16 -0400

    Svelte conversion.

---
+++ ui/src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+import Calibrate from './Calibrate.svelte';
+import About from './About.svelte';
+    
+import Router from 'svelte-spa-router'
+
+const routes = {
+  '/': Calibrate,
+  '/about/': About,
+}
+</script>
+
+<svelte:head>
+  <title>A tool for calibrated probability estimation</title>
+  <html lang="en" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
+</svelte:head>
+
+<body>
+  <Router {routes}/>
+</body>
---
+++ ui/src/NavBar.svelte
@@ -0,0 +1,9 @@
+
+<nav class="navbar navbar-expand navbar-light bg-light">	
+  <a class="navbar-brand p-3" href="/">An Educated Guess</a>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="#/about">About</a>
+    </div>
+  </div>
+</nav>
