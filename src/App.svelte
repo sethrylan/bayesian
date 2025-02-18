91879855a6218cc1e3a4978de7c5fe6dbda5801e
Seth Rylan Gainey <seth.rylan@gmail.com>
2025-02-18 13:51:28 -0500

    chore: replace and cleanup docs folder (#75)
    * chore: replace and cleanup docs folder
    
    * fix: build locations

--- src/App.svelte
+++ src/App.svelte
@@ -14,7 +14,7 @@
   <html lang="en" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link href="https://code.highcharts.com/css/highcharts.css" rel="stylesheet">
-  <link href="build/bundle.css" rel="stylesheet">
+  <link href="bundle.css" rel="stylesheet">
   <link href="global.css" rel="stylesheet">
 </svelte:head>
 
